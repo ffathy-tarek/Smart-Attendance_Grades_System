116001a3d2c32583b7833d4dad37d1bd1470c237
Fathy Tarek <ffathy2244@gmail.com>
2026-03-04 02:17:13 +0200

    feat: add admin routes and basic admin pages

--- frontend-web/src/routes/AppRoutes.jsx
+++ frontend-web/src/routes/AppRoutes.jsx
@@ -3,6 +3,8 @@ import Login from "../pages/Login.jsx";
 import Dashboard from "../pages/Dashboard.jsx";
 import ForgetPassword from "../pages/ForgetPassword.jsx";
 import RequestEmail from "../pages/RequestEmail.jsx"; 
+import AdminDashboard from "../pages/admin/AdminDashboard.jsx";
+import Students from "../pages/admin/Students.jsx";
 
 const AppRoutes = () => {
   return (
@@ -12,6 +14,9 @@ const AppRoutes = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/request-email" element={<RequestEmail />} /> 
+        
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/students" element={<Students />} />
       </Routes>
     </BrowserRouter>
   );
