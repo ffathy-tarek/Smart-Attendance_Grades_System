26f4eb8d114b15bd2b350cf4ed0efb0e5fb14326
David Fady <davidfady665@gmail.com>
2026-03-05 04:34:40 +0200

    add Admin Dashboard(approve/reject and reset pass.)

--- frontend-web/src/routes/AppRoutes.jsx
+++ frontend-web/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Login from "../pages/Login.jsx";
 import Dashboard from "../pages/Dashboard.jsx";
 import ForgetPassword from "../pages/ForgetPassword.jsx";
 import RequestEmail from "../pages/RequestEmail.jsx"; 
+import Admin from "../pages/Admin.jsx";
 
 const AppRoutes = () => {
   return (
@@ -12,6 +13,7 @@ const AppRoutes = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/request-email" element={<RequestEmail />} /> 
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
